1ea0307eac53073b52ff714bbf37865a1acb2570
Duct Tape JS <72471454+ducttapejs@users.noreply.github.com>
2020-10-08 01:00:00 -0400

    some updates

--- ducttape.js
+++ ducttape.js
@@ -3,10 +3,11 @@ var __={
     routes:[],
     loaded:[],
     models:{},
+    components:{},
     loading:0,
     params:{},
     js:null,
-
+    currentScript:document.currentScript,
     /*********************************************
         GET SCRIPT
         
@@ -19,7 +20,7 @@ var __={
             newScript.onerror = function(){ cb(true, null)};
             newScript.onload = function(){ cb(null, true)}; 
         }
-        document.currentScript.parentNode.insertBefore(newScript, document.currentScript);
+        this.currentScript.parentNode.insertBefore(newScript, this.currentScript);
         newScript.src = url;
     },
     
@@ -39,7 +40,6 @@ var __={
         xhr.send();
     },
     
-
     /*********************************************
         LOAD
 
@@ -69,27 +69,40 @@ var __={
         }        
     },
 
-
     /*********************************************
         CALL API
     ******************************************** */
     callAPI:function(url, p){
-        $.support.cors = true;
-            $.ajax({
-                "crossDomain": true,
-                "url": ((url.indexOf("http")===-1) ? this.config.base_api_url : "")+url,
-                "method":((p.method) ? p.method : "GET"),
-                "headers":((p.headers) ? p.headers : {}),
-                "contentType": "application/json; charset=utf-8",
-                "dataType": "json",
-                "data": ((p.data) ? JSON.stringify(p.data) : null),
-                "success":function(r, status){                    
-                    p.response(null,r)
-                },
-                "error":function(x,s,e){                    
-                    p.response(x.responseJSON,null);
-                }
+        var request = new XMLHttpRequest();
+        request.open(((p.method) ? p.method : "GET"), ((url.indexOf("http")===-1) ? this.config.base_api_url : "")+url, true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        if(p.headers){
+            Object.keys(p.headers).forEach(function(key){
+                request.setRequestHeader(key, p.headers[key]);
             });
+        }     
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {              
+                try{
+                    p.response(null,JSON.parse(this.response),this.status);
+                }catch(e){
+                    p.response(null,this.response,this.status);
+                }
+                
+            } else {              
+                p.response({"error":this.statusText, "details":this.response}, null, this.status);  
+            }
+          };
+        request.onerror = function(e) {        
+            p.response({"error":this.statusText, "details":this.response}, null, this.status);
+        };
+        request.ontimeout = function(e) {        
+            p.response("timeout", null, this.status);
+        };
+        request.onabort = function(e) {        
+            p.response("abort", null, this.status);
+        };
+        request.send(((p.data) ? JSON.stringify(p.data) : null));
     },
 
     /*********************************************
@@ -103,6 +116,7 @@ var __={
         var allRoutes=__.routes.slice();
         
         function processRoute(){
+
             if(allRoutes.length>0){
                 var route = allRoutes.shift();                
                 var pathParams=route[0].split(":");
@@ -132,15 +146,13 @@ var __={
     
     },
 
-
     /*********************************************
         ROUTE TO
     ******************************************** */
-    routeTo:function(url){
-        window.history.pushState('', '', url);
-        __.router();
+    routeTo:function(url, includeQS){
+        window.history.pushState('', '', url+((includeQS) ? window.location.search : ""));
+        this.router();
     },
-
 
     /*********************************************
         RENDER SCREEN
@@ -166,26 +178,27 @@ var __={
         return html;
     },
 
-
     /*********************************************
         LOAD COMPONENT
     ******************************************** */
     loadComponent:function(componentId, params, cb){
         var that=this;        
+        console.log("LOAD COMPONENT", componentId);
         if(typeof __.components[componentId]==="undefined"){
-            __.components[componentId]={"html":null, "js":null, "data":params};
+            console.log("GETTING COMPONENT");
+            this.components[componentId]={"html":null, "js":null, "data":params};
             this.getContent(window.location.origin+"/components/"+componentId.toLowerCase()+"/ui.html", function(html){
                 document.getElementById(componentId+"ComponentHolder").innerHTML = html;
-                __.components[componentId].html=html;
-                this.getScript(window.location.origin+"/components/"+componentId.toLowerCase()+"/logic.js", function(){ cb(); });
+                that.components[componentId].html=html;
+                that.getScript(window.location.origin+"/components/"+componentId.toLowerCase()+"/logic.js", function(){ cb(); });
             });
-        }else{
-            __.components[componentId].data=params;
+        }else{            
+            this.components[componentId].data=params;
+            document.getElementById(componentId+"ComponentHolder").innerHTML = this.components[componentId].html;
             cb();
         }
         
     },
-
 
     /*********************************************
         FORM VALIDATOR
@@ -197,7 +210,6 @@ var __={
 
         els.forEach(function(el){
             el.classList.remove("is-invalid");
-            console.log("THIS",el.getAttribute("min") );
             if(el.getAttribute("required")==="required"){            
                 if(el.value.trim().length===0){
                     el.classList.add("is-invalid");
@@ -332,8 +344,15 @@ var __={
 
     ui:{
         loading:{
-            screen:function(){
-                document.getElementById("screen").innerHTML = '<div id="spinner" class="text-align:center;" style="margin-top:40%;"><div class="spinner-border"></div></div>';
+            dim:function(){
+                var html='<div style="width:100%; height:100%; position:fixed; top:0; left:0; background: rgba(0,0,0,0.5); z-index:999999;"></div>';
+            },
+            screen:function(hide){
+                if(hide){
+                    document.getElementById("screen").removeChild(document.getElementById("spinner"));
+                }else{
+                    document.getElementById("screen").innerHTML = '<div id="spinner" class="text-align:center;" style="margin-top:40%;"><div class="spinner-border"></div></div>';
+                }                
             },
             button:function(id, hide){            
                 if(hide){
@@ -344,9 +363,11 @@ var __={
             },
             section:function(id, hide){            
                 if(hide){
-                    document.getElementById(id).removeChild(document.getElementById(id+"Spinner"));
+                    if(document.getElementById(id+"Spinner")){
+                        document.getElementById(id).removeChild(document.getElementById(id+"Spinner"));
+                    }    
                 }else{
-                    document.getElementById(id).innerHTML = document.getElementById(id).innerHTML+'<div id="'+id+'Spinner" style="width:100%; margin:50px 0 50px 0; text-align:center;"><span class="spinner-border mr-2"></span></div>';
+                    document.getElementById(id).innerHTML = document.getElementById(id).innerHTML+'<div id="'+id+'Spinner" style="position:absolute; top:30%; left:0; width:100%; text-align:center;"><span class="spinner-border mr-2"></span></div>';
                 }
             }
         }
@@ -363,7 +384,6 @@ window.isMobileOrTablet = function() {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
   };
-  
 window.location.queryString = function(name){
     var url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
